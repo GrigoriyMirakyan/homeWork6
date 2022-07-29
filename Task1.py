cb118ec50f9886438ba1ff8a57eaa374258ce392
grigoriyMirakyan <grigoriy66660@gmail.com>
2022-07-29 23:00:27 +0300

    Task1_rewrite

--- Task1.py
+++ Task1.py
@@ -1,10 +1,11 @@
 import copy
+
 values = [2, 3, 5, 7, 11, 13, 17, 29, 41, 55]
 transformation = lambda a: copy.deepcopy(a)
-
-
 transformed_values = list(map(transformation, values))
 if values == transformed_values:
     print('Ok')
 else:
     print('Fail')
+
+# Замучился с VSCode, почему то при сохранении автоматически меняет строку с лямбда на функцию def
