53f4bf35f40a05e6f3e347ab803bed8f99fe4ccc
grigoriyMirakyan <grigoriy66660@gmail.com>
2022-07-29 22:51:20 +0300

    Task1

---
+++ Task1.py
@@ -0,0 +1,10 @@
+import copy
+values = [2, 3, 5, 7, 11, 13, 17, 29, 41, 55]
+transformation = lambda a: copy.deepcopy(a)
+
+
+transformed_values = list(map(transformation, values))
+if values == transformed_values:
+    print('Ok')
+else:
+    print('Fail')
